f71c481c5ba7447732a3e3afb4c4117d352a424b
AmedeoPasanisi <amedeopasanisi97@gmail.com>
2022-03-25 17:37:44 +0100

    relative dimensions implementation

--- sketches/sketch-01.js
+++ sketches/sketch-01.js
@@ -1,22 +1,26 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 600, 600 ]
+  dimensions: [ 1080, 1080 ]
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+    context.lineWidth = width * 0.01;
 
-    const w = 60;
-    const h = 60;
-    const gap = 20;
+    const w = width * 0.10;
+    const h = height * 0.10;
+    const gap = width * 0.03;
+    const ix = width * 0.17; // initial x (and y)
+    const iy = width * 0.17;
     let x, y;
+    const off = width * 0.05; // offset
     for (let i = 0; i < 5; i++) {
       for (let j = 0; j < 5; j++) {
-        x = 100 + (w + gap) * i;
-        y = 100 + (h + gap) * j;
+        x = ix + (w + gap) * i;
+        y = iy + (h + gap) * j;
     
         context.beginPath();
         context.rect(x, y, w, h);
@@ -24,7 +28,7 @@ const sketch = () => {
 
         if (Math.random() > 0.5) {
           context.beginPath();
-          context.rect(x + 10, y + 10, w - 20, h - 20);
+          context.rect(x + off / 2, y + off / 2, w - off, h - off);
           context.stroke();
         }
       }
